6267a4a3a9b52191cbb137cc983405f5d677c6aa
Illusion52 <andreytalaba52@gmail.com>
2022-10-21 17:49:48 +0300

    Add existing files

---
+++ src/pages/signin.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import SignIn from '../components/Signin'
+import ScrollToTop from '../components/ScrollToTop'
+
+const SinginPage = () => {
+  return (
+    <>
+    <ScrollToTop />
+      <SignIn  />
+    </>
+  )
+}
+
+export default SinginPage
